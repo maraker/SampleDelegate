027c14115a8edbc1c9129b111667654f04c87bc2
Markku Åkerfelt <maraker@icloud.com>
2015-03-09 21:22:04 +0200

    Initial Commit

---
+++ SampleDelegate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SampleDelegate
+//
+//  Created by Markku Åkerfelt on 09/03/15.
+//  Copyright (c) 2015 MarakerSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
